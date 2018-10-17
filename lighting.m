c3529ff6c676f33c0f100a6ae76b2d062dc5eb11
Seagal Putra <seagalputra@Seagals-MacBook-Air.local>
2018-10-17 05:59:54 -0700

    Plot sruface normal into graph

--- lighting.m
+++ lighting.m
@@ -6,9 +6,13 @@ close all;
 % img = rgb2gray(img);
 
 img = imread('datasets/4/4_l1c2.png');
+% img = imread('datasets/32/32_l5c1.png');
 img = cropImg(img);
 
 newImg = imgPadding(img);
 [resVectorDirection, magnitudeNormal, actualNormal] = estSurfNorm(newImg);
 
+imshow(img);
+hold on;
 quiver(actualNormal(:,1), actualNormal(:,2), actualNormal(:,3), actualNormal(:,4));
+axis on;
