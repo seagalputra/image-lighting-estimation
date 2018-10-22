a0d14c9683fc1e907fb37a4ed2cb564e403b5f44
Dwiferdio Seagal Putra <seagalputra@gmail.com>
2018-10-22 20:14:00 +0700

    Split image into 9x9 pixel block

--- lighting.m
+++ lighting.m
@@ -2,6 +2,8 @@ clear;
 clc;
 close all;
 
+%% Load image and convert to grayscale image
+
 % Using image demos in MATLAB folder
 img = imread('toysnoflash.png');
 
@@ -11,12 +13,27 @@ if (size(img, 3) == 3)
 end
 
 img = imcrop(img);
-[height, width] = size(img);
+
+% maintain image in 1:1 size ratio
+sizeImg = size(img);
+overPixel = mod(sizeImg, 9);
+img = img(1:sizeImg(1)-overPixel(1),1:sizeImg(2)-overPixel(2));
+
+%% Compute surface normal
 
 newImg = imgPadding(img);
 [resVectorDirection, magnitudeNormal, actualNormal] = estSurfNorm(newImg);
 
-imshow(img);
-hold on;
-quiver(actualNormal(:,1), actualNormal(:,2), actualNormal(:,3), actualNormal(:,4));
-axis on;
+% imshow(img);
+% hold on;
+% quiver(actualNormal(:,1), actualNormal(:,2), actualNormal(:,3), actualNormal(:,4));
+% axis on;
+
+%% Divide image into k small planes every plane contains 9x9 pixels
+
+plane = floor(height * width/81);
+sz = size(img);
+chunkSize = [9 9];
+sc = sz ./ chunkSize;
+
+imgSplit = mat2cell(img, chunkSize(1) * ones(sc(1),1), chunkSize(2) * ones(sc(2),1));
