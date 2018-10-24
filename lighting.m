86794d22508803078e0af49eaedcc1e5cf64e5d3
Dwiferdio Seagal Putra <seagalputra@gmail.com>
2018-10-24 07:29:24 +0700

    Construct block matrix M and vector b

--- lighting.m
+++ lighting.m
@@ -19,20 +19,41 @@ sizeImg = size(img);
 overPixel = mod(sizeImg, 9);
 img = img(1:sizeImg(1)-overPixel(1),1:sizeImg(2)-overPixel(2));
 
-%% Compute surface normal
-
-newImg = imgPadding(img);
-[resVectorDirection, magnitudeNormal, actualNormal] = estSurfNorm(newImg);
-
-% imshow(img);
-% hold on;
-% quiver(actualNormal(:,1), actualNormal(:,2), actualNormal(:,3), actualNormal(:,4));
-% axis on;
-
-%% Divide image into k small planes every plane contains 9x9 pixels
+%% Divide image into k small planes every plane contains 9x9 pixels 
 
 sz = size(img);
 chunkSize = [9 9];
 sc = sz ./ chunkSize;
-
 imgSplit = mat2cell(img, chunkSize(1) * ones(sc(1),1), chunkSize(2) * ones(sc(2),1));
+
+%% Compute surface normal
+
+M = {};
+b = [];
+for i = 1:size(imgSplit, 1)
+    for j = 1:size(imgSplit, 2)
+        tempImg = imgPadding(imgSplit{i,j});
+        [resVectorDirection, magnitudeNormal, actualNormal] = estSurfNorm(tempImg);
+        vecDirection{i,j} = resVectorDirection;
+        % Define vector b
+        b = [b reshape(imgSplit{i,j}',[],1)];
+        % Construct M matrix
+        M{end+1} = [resVectorDirection ones(81, 1)];
+    end
+end
+
+%% Obtain lighting direction
+
+% create block matrix of M
+blockM = blkdiag(M{:});
+
+% create vector b
+b = reshape(b,[],1);
+
+%% Estimate lighting parameter using error function
+
+% split array of vector direction into cell
+% vectorSz = size(resVectorDirection);
+% vectorChunkSize = sz;
+% vectorSc = vectorSz ./ vectorChunkSize;
+% vecDirection = mat2cell(resVectorDirection, vectorChunkSize(1) * ones(vectorSc(1), 1), 2);
