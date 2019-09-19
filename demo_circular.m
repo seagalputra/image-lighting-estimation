863fba72615294f85055efb1cf5bec881c037212
seagalputra <seagalputra@gmail.com>
2019-09-19 14:36:28 +0700

    rename demo files

--- demo_circular.m
+++ demo_circular.m
@@ -1,8 +1,8 @@
 % This is a demo for estimating light direction from single image using
-% object boundary
+% object boundary (for circular shape object)
 clear; clc; close all;
 
-img = imread('data/4.JPG');
+img = imread('data/1.JPG');
 % convert RGB image into Grayscale
 img_gray = rgb2gray(img);
 
@@ -10,23 +10,36 @@ img_gray = rgb2gray(img);
 gaps = 10; % how many gaps between point to use
 n_points = 3; % how many points in a surface
 offset = 15; % offset to obtain intensity
-length = 3000; % length to plot line
+len = 3000; % length to plot line
 
-% segment image using k-means clustering
-[bw, mask] = segment_image(img);
+% SEGMENTATION PARAMETER
+seg_choice = 1;
+n_colors = 2;
+num_replicates = 3;
+size_threshold = 1000;
 
-% segment image using YCbCr color space
-% [bw, mask] = color_threshold(img);
+disp('Perform image segmentation...');
+if (seg_choice == 1)
+    % segment image using k-means clustering
+    disp('(using k-means clustering)');
+    bw = kmeans_seg(img, n_colors);
+elseif (seg_choice == 2)
+    disp('(using YCbCr color space)');
+    % segment image using YCbCr color space
+    bw = color_threshold(img);
+end
+
 % create bounding box and area using binary mask
 % filter with largest area
-[center, radius, box_props] = bwprops(bw);
+[center, radius, box_props] = bwprops(bw, size_threshold);
 
+%% Calculate light source direction for every object
+disp('Estimate lighting direction...');
 imshow(img);
 hold on;
-% calculate light source direction for every object
 for i = 1:size(center,1)
-    % suppose the object is circular
-    [x, y, nx, ny] = fit_circular(center(i,:), radius(i,:));
+    % find points at boundary object and calculate surface normal
+    [x, y, nx, ny, theta] = fit_circular(center(i,:), radius(i,:));
     
     % before calculating the light source direction, split normal surface
     % into several plane
@@ -45,16 +58,16 @@ for i = 1:size(center,1)
     
     % plot surface normal every points
     plot(x, y, 'r.');
-    for j = 1:length(theta)
+    for j = 1:size(x,2)
         line( [x(j) x(j)+nx(j)], [y(j) y(j)+ny(j)] );
     end
     hold on;
     % plot light source direction from center object
-    line([center(i,1) center(i,1)+L(1)*length], [center(i,2) center(i,2)+L(2)*length], 'Color', 'red', ...
+    line([center(i,1) center(i,1)+L(1)*len], [center(i,2) center(i,2)+L(2)*len], 'Color', 'red', ...
         'LineWidth', 2);
 end
 
-% draw rectangle
+%% Draw bounding box
 % imshow(img);
 % for i = 1:size(box_props,1)
 %     % crop object in image
