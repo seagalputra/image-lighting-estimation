8f8609b88f40b1b08f0a9a721b9d0ce409d3384d
seagalputra <seagalputra@gmail.com>
2019-09-23 05:05:36 +0700

    Fix image load data

--- load_data.m
+++ load_data.m
@@ -2,22 +2,32 @@ clear; clc; close all;
 
 list_dir = dir('data/syn/original/**/*.JPG');
 
+apple = {};
+apple_ball = {};
+apple_ball_cube = {};
+ball = {};
+ball_cube = {};
+cube = {};
 for i = 1:size(list_dir,1)
     % create specific array
     path_split = strsplit(list_dir(i).folder, '\');
     group = path_split{end};
+    disp(['Load ', group, ' image']);
     switch group
         case 'apple'
-            apple{i} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
+            apple{end+1} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
         case 'apple-ball'
-            apple_ball{i} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
+            apple_ball{end+1} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
         case 'apple-ball-cube'
-            apple_ball_cube{i} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
+            apple_ball_cube{end+1} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
         case 'ball'
-            ball{i} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
+            ball{end+1} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
         case 'ball-cube'
-            ball_cube{i} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
+            ball_cube{end+1} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
         case 'cube'
-            cube{i} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
+            cube{end+1} = imread(fullfile(list_dir(i).folder, list_dir(i).name));
     end
 end
+
+save('image_data.mat', 'apple', 'apple_ball', 'apple_ball_cube', 'ball', ...
+    'ball_cube', 'cube');
